e523d78e7207a8f5a23b8876c5ddd0ade632b892
kaczmarekkacper <kaaczmarekkacper@gmail.com>
2019-11-05 23:00:45 +0100

    DMC

--- DMC.m
+++ DMC.m
@@ -1,27 +1,32 @@
 function [y, u] = DMC(Gz, D, N, Nu, lambda, hlin, ulin, Tk, yzad)
+%DMC(Gz, 2393, 600, 1, 1, [h1p, h2p], [F1p, FDp], 4000, h2p+0.5);
 % Gz - transmitancja dyskretna
-% D - hotyzont dynamiki, (1200)
-% N - horyzont predykcji,
-% Nu - d³ugoœæ horyzontu sterownia
-% lambda - parametr regulatora (1200)
+% D - hotyzont dynamiki, (2393)
+% N - horyzont predykcji, (600 500)
+% Nu - d³ugoœæ horyzontu sterownia (1)
+% lambda - parametr regulatora (1)
 % hlin - 2x1 lub 1x2 wektor wartoœci h pracy
 % ulin - -||- u pracy
 % Tk - d³ugoœæ symulacji
 % yzad - wartoœæ zadana
 
 
+%Prepare ODE options.
+options = odeset('RelTol',1e-8,'AbsTol',1e-10);
+
 u(1:(Gz.InputDelay(1))) = 0;
 y1(1:(Gz.InputDelay(1))) = 0;
 u((Gz.InputDelay(1)):((Gz.InputDelay(1))+D)) = 1;
-s = zeros(D, 1);
 
 
 % Wyznaczanie wektora s
-for k=(Gz.InputDelay(1))+1:(Gz.InputDelay(1))+D
-    y1(k)=-Gz.Denominator{1}(2)*y1(k-1)-Gz.Denominator{1}(3)*y1(k-2)+Gz.Numerator{1}(2)*u(k-((Gz.InputDelay(1))-1))+Gz.Numerator{1}(3)*u(k-(Gz.InputDelay(1)));
-    s(k-(Gz.InputDelay(1)))=y1(k);
-end
+% for k=(Gz.InputDelay(1))+1:(Gz.InputDelay(1))+D
+%     y1(k)=-Gz.Denominator{1}(2)*y1(k-1)-Gz.Denominator{1}(3)*y1(k-2)+Gz.Numerator{1}(2)*u(k-((Gz.InputDelay(1))-1))+Gz.Numerator{1}(3)*u(k-(Gz.InputDelay(1)));
+%     s(k-(Gz.InputDelay(1)))=y1(k);
+% end
 
+s = step(Gz);
+s = s(:,:,1);
 psi = eye(N);
 LAMBDA = eye(Nu)*lambda;
 
@@ -54,15 +59,17 @@ yzad=yzad';
 deltaup=zeros(1,D-1)';
 uk= ones((Gz.InputDelay(1)),1).*ulin(1);
 y = ones(Tk, 1).*hlin(2);
-h1 = hlin(1);
-
-
+% h1 = 0;
+h = [hlin(1), hlin(2)];
 % g³ówna pêtla symulacji
 for k=2:Tk
     if k > (Gz.InputDelay(1))
-        h = stateFunction(0,[h1, y(k-1)],uk(k - (Gz.InputDelay(1))), ulin(2));
-        h1 = h1 + h(1);
-        y(k) = y(k-1) + h(2);
+        stateHandler = @(t,x) stateFunction(t,x,uk(k - (Gz.InputDelay(1))), ulin(2));
+        [t, h] = ode45(stateHandler,[0 Gz.Ts],h(end, :), options);
+        y(k) = h(end,2);
+%         h = stateFunction(0,[h1, y(k)],uk(k - (Gz.InputDelay(1))), ulin(2));
+%         h1 = h(1) + h1;
+%         y(k) = y(k-1) + h(2);
     end
     
     
@@ -87,12 +94,15 @@ for k=2:Tk
 end
 
 figure;
-plot( uk, 'g');
-hold on;
-stairs(yzad, 'b');
-stairs(y, 'r');
+subplot(2,1,2);
+stairs( uk, 'g');
 title(strcat('Dzialanie regulatora dla nastaw D=', num2str(D), ' N =', num2str(N), ' Nu=', num2str(Nu), ' lambda=',num2str(lambda)));
-grid on;
-legend('Sterowanie', 'Wyjœcie zadane', 'wyjœcie regulatora', 'Location', 'east');
+legend('Sterowanie');
+subplot(2,1,1);
+stairs(ones(Tk,1).*yzad(1), 'b');
+hold on;
+stairs(y, 'r');
+legend('Wyjœcie zadane', 'wyjœcie regulatora', 'Location', 'east');
 xlabel('k');
 ylabel('y/u');
+hold off;

6ad67dd8a7fc75258a60dec4e3a9da08cacf970c
kaczmarekkacper <kaaczmarekkacper@gmail.com>
2019-11-13 15:10:43 +0100

    Kosmetyczne

---
+++ DMCNoiseAndWithoutSimulation.m
@@ -0,0 +1,87 @@
+load Gz.mat;
+D = 2393;
+N = 600;
+Nu = 1;
+lambda =1;
+yzad = h2p+0.5;
+Tk = 4000;
+
+Fd = [ ones(1,Tk/2).*FDp, ones(1,Tk/2).*(FDp+5) ] ;
+
+options = odeset('RelTol',1e-8,'AbsTol',1e-10);
+
+%%Prepare DMC regulator with noise.
+dmcnoise = DMCRegWithNoise(Gz,D, N, Nu, lambda);
+dmcnoise.reset(F1p, FDp);
+dmcnoise.setValue(yzad);
+
+%Prepare DMC regulator.
+dmc = DMCReg(Gz,D, N, Nu, lambda);
+dmc.reset(F1p);
+dmc.setValue(yzad);
+
+uk= ones((Gz.InputDelay(1)),1).*F1p;
+y = ones(Tk, 1).*h2p;
+h = [h1p, h2p];
+
+%Main simulation loop.
+for k=2:Tk
+    if k > (Gz.InputDelay(1))
+        stateHandler = @(t,x) stateFunction(t,x,uk(k - (Gz.InputDelay(1))), Fd(k));
+        [t, h] = ode45(stateHandler,[0 Gz.Ts],h(end, :), options);
+        y(k) = h(end,2);
+    end
+    uk(k) = dmcnoise.countValue(y(k), Fd(k));
+end
+
+figure();
+subplot(2,2,1);
+plot(ones(Tk,1).*(yzad), 'b-');
+
+hold on;
+stairs(y, 'r');
+legend('Wyjœcie zadane', 'Wyjœcie regulatora', 'Location', 'southeast');
+title(strcat('Regulator DMC z uwzglêdnieniem zak³óceñ'));
+xlabel('k');
+ylabel('y');
+subplot(2,2,3);
+stairs( uk, 'g');
+hold on;
+stairs(Fd, 'r');
+legend('Sterowanie', 'Zak³ócenie', 'Location', 'east');
+xlabel('k');
+ylabel('u');
+hold off;
+
+uk= ones((Gz.InputDelay(1)),1).*F1p;
+y = ones(Tk, 1).*h2p;
+h = [h1p, h2p];
+
+%Main simulation loop.
+for k=2:Tk
+    if k > (Gz.InputDelay(1))
+        stateHandler = @(t,x) stateFunction(t,x,uk(k - (Gz.InputDelay(1))), Fd(k));
+        [t, h] = ode45(stateHandler,[0 Gz.Ts],h(end, :), options);
+        y(k) = h(end,2);
+    end
+    uk(k) = dmc.countValue(y(k));
+end
+
+
+subplot(2,2,2);
+plot(ones(Tk,1).*(yzad), 'b-');
+hold on;
+stairs(y, 'r');
+legend('Wyjœcie zadane', 'Wyjœcie regulatora', 'Location', 'southeast');
+xlabel('k');
+ylabel('y');
+title(strcat('Regulator DMC konwencjonalny'));
+subplot(2,2,4);
+stairs( uk, 'g');
+hold on;
+stairs(Fd, 'r');
+legend('Sterowanie', 'Zak³ócenie', 'Location', 'east');
+xlabel('k');
+ylabel('u');
+hold off;
+print(sprintf('DMCCompareSkok=%.1f.pdf', round(Fd(end)-Fd(1), 1)), '-dpdf');
--- DMCSimulation.m
+++ DMCSimulation.m
@@ -5,14 +5,11 @@ Nu = 1;
 lambda =1;
 yzad = h2p+0.5;
 Tk = 4000;
-Fd = [ ones(1,Tk/2).*14, ones(1,Tk/2).*16 ] ;Tk = 4000;
 
 options = odeset('RelTol',1e-8,'AbsTol',1e-10);
-
-%%Prepare DMC regulator.
-dmc = DMCRegWithNoise(Gz,D, N, Nu, lambda);
-dmc.reset(F1p, FDp);
-dmc.setValue(h2p+0.5);
+dmc = DMCReg(Gz,D, N, Nu, lambda);
+dmc.reset(F1p);
+dmc.setValue(yzad);
 
 uk= ones((Gz.InputDelay(1)),1).*F1p;
 y = ones(Tk, 1).*h2p;
@@ -21,17 +18,17 @@ h = [h1p, h2p];
 %Main simulation loop.
 for k=2:Tk
     if k > (Gz.InputDelay(1))
-        stateHandler = @(t,x) stateFunction(t,x,uk(k - (Gz.InputDelay(1))), Fd(k));
+        stateHandler = @(t,x) stateFunction(t,x,uk(k - (Gz.InputDelay(1))), FDp);
         [t, h] = ode45(stateHandler,[0 Gz.Ts],h(end, :), options);
         y(k) = h(end,2);
     end
-    uk(k) = dmc.countValue(y(k), Fd(k));
+    uk(k) = dmc.countValue(y(k));
 end
 
 figure();
 subplot(2,1,1);
 plot(ones(Tk,1).*(yzad), 'b-');
-title(strcat('Regulator z uwzglêdnieniem zak³óceñ dla nastaw D=', num2str(D), ' N =', num2str(N), ' Nu=', num2str(Nu), ' lambda=',num2str(lambda)));
+title(strcat('Dzialanie regulatora dla nastaw D=', num2str(D), ' N =', num2str(N), ' Nu=', num2str(Nu), ' lambda=',num2str(lambda)));
 hold on;
 stairs(y, 'r');
 legend('Wyjœcie zadane', 'Wyjœcie regulatora', 'Location', 'east');
@@ -42,6 +39,6 @@ stairs( uk, 'g');
 legend('Sterowanie');
 xlabel('k');
 ylabel('u');
-title(strcat('Skok wartoœci zadanej =', num2str(yzad-h2p), ' Skok zak³ócenia o ', num2str(Fd(end)-Fd(1))));
+title(strcat('Skok wartoœci zadanej =', num2str(yzad-h2p)));
 hold off;
-%print(sprintf('DMCNoiseskok=%.1f.pdf', round(yzad-h2p, 1)), '-dpdf');
+%print(sprintf('DMCskok=%.1f.pdf', round(yzad-h2p, 1)), '-dpdf');
